01852d64b9ade93b498b1a087c87568489fd313a
Ernando <ernando.santana@totemti.com.br>
2023-06-18 19:12:36 -0300

    Mapeamento e conexão com banco de dados

---
+++ src/main/java/br/com/academy/model/Aluno.java
@@ -0,0 +1,72 @@
+package br.com.academy.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Aluno {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String nome;
+
+    private Enum curso;
+
+    private String matricula;
+
+    private Enum status;
+
+    private String turno;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Enum getCurso() {
+        return curso;
+    }
+
+    public void setCurso(Enum curso) {
+        this.curso = curso;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public Enum getStatus() {
+        return status;
+    }
+
+    public void setStatus(Enum status) {
+        this.status = status;
+    }
+
+    public String getTurno() {
+        return turno;
+    }
+
+    public void setTurno(String turno) {
+        this.turno = turno;
+    }
+}
